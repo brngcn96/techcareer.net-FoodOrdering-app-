52d06ec8dec87b51ac7de9d20fdcfb473f6ff3ac
Baran Göcen <barangokselgocen@icloud.com>
2021-12-17 01:34:19 +0300

    v8

---
+++ FoodOrderingApp/WelcomeViewController.swift
@@ -0,0 +1,33 @@
+//
+//  WelcomeViewController.swift
+//  FoodOrderingApp
+//
+//  Created by Baran Göcen on 17.12.2021.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func buttonYemeklereGit(_ sender: Any) {
+        performSegue(withIdentifier: "toYemekler", sender: nil)
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
